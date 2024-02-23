25c0791dc960964048177708ca6da00f1c9fc3e7
QuyHoa <160729488+QuyHoa@users.noreply.github.com>
2024-02-23 08:58:46 +0700

    EqualsMethod

---
+++ src/main/java/com/mycompany/app/EqualsMethod.java
@@ -0,0 +1,15 @@
+/*----------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
+package com.mycompany.app;
+
+public class EqualsMethod {
+    public static void main(String[] args) {
+    Integer integer = Integer.valueOf(47);
+    Integer integer2 = Integer.valueOf(47);
+    System.out.println(integer.equals(integer2));
+   
+    }
+   } 
