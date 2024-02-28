12397855418abf0aef74a7b770e0ab9df69f2629
QuyHoa <160729488+QuyHoa@users.noreply.github.com>
2024-02-28 10:30:02 +0000

    BTTH1

--- src/main/java/com/mycompany/app/Slide6.java
+++ src/main/java/com/mycompany/app/Slide6.java
@@ -7,7 +7,7 @@ class Number {
     public static void main(String[] args) {
     Number n1 = new Number();
     Number n2 = new Number();
-    n1.i = 2;
+    n1.i = 3;
     n2.i = 5;
     n1.i = n2.i;
     n2.i = 10;// what is n1.i?
