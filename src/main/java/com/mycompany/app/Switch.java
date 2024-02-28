55a5890758d1e1275fb216a1cc32100079760972
QuyHoa <160729488+QuyHoa@users.noreply.github.com>
2024-02-28 10:54:31 +0000

    Case

---
+++ src/main/java/com/mycompany/app/Switch.java
@@ -0,0 +1,32 @@
+package com.mycompany.app;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        // Bai 1
+        int day1 = 2;
+        switch (day1) {
+            case 1:
+                System.out.println("Saturday");
+                break;
+            case 2:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+        // Bai 2
+        int day2 = 4;
+        switch (day2) {
+            case 1:
+                System.out.println("Saturday");
+                break;
+            case 2:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Weekend");
+        }
+    }
+}
