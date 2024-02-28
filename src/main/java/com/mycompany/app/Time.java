dace9b07439b8b07bd8f9a0183b09d3fa6ada909
QuyHoa <160729488+QuyHoa@users.noreply.github.com>
2024-02-28 10:14:04 +0000

    Assignment 1

---
+++ src/main/java/com/mycompany/app/Time.java
@@ -0,0 +1,76 @@
+package com.mycompany.app;
+
+//This: refer to the current object and method in the program
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time() {
+        setTime(0, 0, 0);
+    }
+
+    public Time(int h) {
+        setTime(h, 0, 0);
+    }
+
+    public Time(int h, int m) {
+        setTime(h, m, 0);
+    }
+
+    public Time(int h, int m, int s) {
+        setTime(h, m, s);
+    }
+
+    public Time setTime(int h, int m, int s) {
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
+        return this;
+    }
+
+    public Time setHour(int h) {
+        hour = ((h >= 0 && h < 24) ? h : 0);
+        return this;
+    }
+
+    public Time setMinute(int m) {
+        minute = ((m >= 0 && m < 60) ? m : 0);
+        return this;
+    }
+
+    public Time setSecond(int s) {
+        second = ((s >= 0 && s < 60) ? s : 0); // Fixed condition from 24 to 60
+        return this;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    
+    public String toString() {
+        return ((hour == 12 || hour == 0) ? 12 : hour % 12) +
+                ":" + (minute < 10 ? "0" : "") + minute +
+                ":" + (second < 10 ? "0" : "") + second +
+                (hour < 12 ? " AM" : " PM");
+    }
+
+    public class TestTime {
+      public static void main(String[] args) {
+      Time t1 = new Time();
+      Time t2 = new Time(20, 3, 45);
+      t1.setHour(7).setMinute(32).setSecond(23);
+      System.out.println("t1 is " + t1);
+      System.out.println("t2 is " + t2);
+      }
+     }
+}
