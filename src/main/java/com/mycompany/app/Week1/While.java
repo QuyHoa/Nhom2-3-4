db83fb555fb57469ffa965570a94b9413e6ab456
DTKDuien <120620061+DTKDuien@users.noreply.github.com>
2024-02-29 10:15:12 +0000

    Kduien commit

--- src/main/java/com/mycompany/app/Week1/While.java
+++ src/main/java/com/mycompany/app/Week1/While.java
@@ -7,5 +7,46 @@ public class While {
             System.out.println(i);
             i++;
         }
+    
+
+int j = 1;
+do
+ {
+  System.out.println(i);
+  j++;
+}
+while
+ (j < 6);
+
+
+for (int x = 0; x < 5; x++) {
+    System.out.println("Yes");
+
+
+
+}
+
+String[] cars = {"Volvo", "BMW", "Ford"};
+for
+ (String k : cars) {
+    System.out.println(k);
+}
+for (int y = 0; y < 10; y++) {
+    if (y == 5) {
+      
+  break
+  ;
     }
+    System.out.println(y);
+  }
+for (int z = 0; z < 10; z++) {
+    if (z == 4) {
+      
+  continue
+  ;
+    }
+    System.out.println(z);
+  }
+ 
+  }
 }
