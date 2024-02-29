4e9ff6d0884015882f9a78dd1a577e887a498219
NguyetDui <160729524+NguyetDui@users.noreply.github.com>
2024-02-29 10:55:05 +0000

    nguyet commit lass

---
+++ src/main/java/com/mycompany/app/Week2/Classes.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.Week2;
+
+public class Classes {
+    public static void main(String[] args) {
+        MyClass myObj = new MyClass();
+        String x = "Nguyet commit";
+        System.out.println(x);
+    }
+}
+
+class MyClass {
+    
+}
