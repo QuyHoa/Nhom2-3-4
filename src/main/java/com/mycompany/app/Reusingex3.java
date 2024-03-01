cc4f39c7ac1352812b41d0be1e04dc823c927beb
NguyetDui <160729524+NguyetDui@users.noreply.github.com>
2024-03-01 02:09:55 +0000

    nguyet commit

---
+++ src/main/java/com/mycompany/app/Reusingex3.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+class Reusingex3 {
+    public Reusingex3() {
+        System.out.println("Reusingex3 constructor");
+    }
+}
+
+class DerivedClass extends Reusingex3 {
+    
+}
+
+class Cartoon extends DerivedClass {
+   
+}
+
+ class Main {
+    public static void main(String[] args) {
+        new Cartoon(); 
+    }
+}
