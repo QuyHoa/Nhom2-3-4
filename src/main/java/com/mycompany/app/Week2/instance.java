8f8f4381073194c9963f07fd1b0eb278ebeed19d
NguyetDui <160729524+NguyetDui@users.noreply.github.com>
2024-03-01 01:51:45 +0000

    N

---
+++ src/main/java/com/mycompany/app/Week2/instance.java
@@ -0,0 +1,11 @@
+import com.mycompany.app.Week2.Package;
+
+public class instance {
+    public static void main(String[] args) {
+        // Tạo một thể hiện của lớp Package
+        Package myPackage = new Package();
+
+        // Gọi phương thức displayMessage của lớp Package
+        myPackage.displayMessage();
+    }
+}
---
+++ src/main/java/com/mycompany/app/Week2/package.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.Week2;
+
+public class package {
+    public void displayMessage() {
+        System.out.println("This is a message from MyClass.");
+    }
+}
