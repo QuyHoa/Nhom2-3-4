0cb3dbb3a5f0499732d0d811086fe4c2ef85c728
NguyetDui <160729524+NguyetDui@users.noreply.github.com>
2024-03-06 15:57:19 +0000

    n

---
+++ src/main/java/com/mycompany/app/Week2/Ex20.java
@@ -0,0 +1,32 @@
+package com.mycompany.app.Week2;
+
+class WithFinals {
+    final void f() {
+        System.out.println("WithFinals.f()");
+    }
+
+    void g() {
+        System.out.println("WithFinals.g()");
+    }
+}
+
+class OverridingPrivate extends WithFinals {
+    
+}
+
+class OverridingPrivate2 extends OverridingPrivate {
+    
+
+    public void g() {
+        System.out.println("OverridingPrivate2.g()");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        OverridingPrivate2 op2 = new OverridingPrivate2();
+        
+        op2.f();
+        op2.g();
+    }
+}
