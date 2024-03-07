112ba789b1441b007a18f5a091df933c80917cd4
DTKDuien <120620061+DTKDuien@users.noreply.github.com>
2024-03-07 09:39:37 +0000

    kduien cmt

---
+++ src/main/java/com/mycompany/app/Week2/Note.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.Week2;
+
+public interface Note {
+
+}
---
+++ src/main/java/com/mycompany/app/Week2/PolymorphismEX6.java
@@ -0,0 +1,96 @@
+
+package com.mycompany.app.Week2;
+class Instrument {
+    public String toString() {
+        return "Instrument";
+    }
+    void play(String note) {
+        System.out.println("Instrument.play() " + note);
+    }
+    void adjust() {
+        System.out.println("Adjusting Instrument");
+    }
+}
+
+class Wind extends Instrument {
+    public String toString() {
+        return "Wind";
+    }
+    void play(String note) {
+        System.out.println("Wind.play() " + note);
+    }
+    void adjust() {
+        System.out.println("Adjusting Wind");
+    }
+}
+
+class Percussion extends Instrument {
+    public String toString() {
+        return "Percussion";
+    }
+    void play(String note) {
+        System.out.println("Percussion.play() " + note);
+    }
+    void adjust() {
+        System.out.println("Adjusting Percussion");
+    }
+}
+
+class Stringed extends Instrument {
+    public String toString() {
+        return "Stringed";
+    }
+    void play(String note) {
+        System.out.println("Stringed.play() " + note);
+    }
+    void adjust() {
+        System.out.println("Adjusting Stringed");
+    }
+}
+
+class Brass extends Wind {
+    public String toString() {
+        return "Brass";
+    }
+    void play(String note) {
+        System.out.println("Brass.play() " + note);
+    }
+    void adjust() {
+        System.out.println("Adjusting Brass");
+    }
+}
+
+class Woodwind extends Wind {
+    public String toString() {
+        return "Woodwind";
+    }
+    void play(String note) {
+        System.out.println("Woodwind.play() " + note);
+    }
+}
+
+public class PolymorphismEX6 {
+    public static void tune(Instrument i, String note) {
+        i.play(note);
+    }
+    public static void tuneAll(Instrument[] e, String note) {
+        for (Instrument i : e)
+            tune(i, note);
+    }
+    public static void main(String[] args) {
+        Instrument[] orchestra = {
+            new Wind(),
+            new Percussion(),
+            new Stringed(),
+            new Brass(),
+            new Woodwind()
+        };
+        String note = "C#"; // Example note
+        tuneAll(orchestra, note);
+
+        // Printing Instrument objects using System.out.println() (without any casting)
+        for (Instrument instrument : orchestra) {
+            System.out.println(instrument);
+        }
+    }
+}
