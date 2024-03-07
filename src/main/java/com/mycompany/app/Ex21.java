5672e82cce4eb9a11649ec623c13a2a7d67bdb69
NguyetDui <160729524+NguyetDui@users.noreply.github.com>
2024-03-07 03:09:13 +0000

    e

---
+++ src/main/java/com/mycompany/app/Ex21.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+public class Ex21 {
+    
+    public final void finalMethod() {
+        System.out.println("Final method.");
+    }
+}
+
+class ExClass extends Ex21 {
+    
+}
+
+class Main {
+    public static void main(String[] args) {
+        Ex21 parent = new Ex21();
+        parent.finalMethod(); 
+    }
+}
+
